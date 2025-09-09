75e58617dd8742c5f9f6ec4c9b82bba3246182f7
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-09-09 10:54:00 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Core\Routing\Routes;
 use App\Middlewares\BlockEdge;
-
-
 Routes::get("/php-expert/Mini%20Projects/Micro%20Framework%20Laravel/" , "HomeController@index" , [BlockEdge::class]);
 Routes::get("/php-expert/Mini%20Projects/Micro%20Framework%20Laravel/colors/red" , "ColorsController@red");
 Routes::get("/php-expert/Mini%20Projects/Micro%20Framework%20Laravel/colors/green" , "ColorsController@green");
